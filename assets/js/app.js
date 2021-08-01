30a09ce14afdbc52ddf9807b2936c7ecce2186de
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-08-01 20:36:23 +0200

    Add images which won t show when screen is smaller than 1000px and will be replaced by carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,36 @@
 const app = new Vue({
     el: '#app',
     data: {
-
+        window: {
+            width: 0,
+            height: 0
+        },
+        whyUs: [
+            {
+                'image': './assets/img/04-perchè-scegliere-noi/imm-lab-01.png',
+                'text': 'Esami della vista'
+            },
+            {
+                'image': './assets/img/04-perchè-scegliere-noi/imm-lab-02.png',
+                'text': 'I migliori accessori'
+            },
+            {
+                'image': './assets/img/04-perchè-scegliere-noi/imm-lab-03.png',
+                'text': 'Articoli specifici'
+            }
+        ],
     },
-
-
+    created() {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.handleResize);
+    },
+    methods: {
+        handleResize() {
+            this.window.width = window.innerWidth;
+            this.window.height = window.innerHeight;
+        }
+    }
 })
