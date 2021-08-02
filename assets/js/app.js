4149681eb5a6fedb521cf9c85b835a3386688657
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-08-02 19:15:01 +0200

    Implemented carousel on smaller view for why us section

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,8 @@ const app = new Vue({
                 'text': 'Articoli specifici'
             }
         ],
+        images: ['./assets/img/04-perchè-scegliere-noi/imm-lab-01.png', './assets/img/04-perchè-scegliere-noi/imm-lab-02.png', './assets/img/04-perchè-scegliere-noi/imm-lab-03.png'],
+        counter: 0,
     },
     created() {
         window.addEventListener('resize', this.handleResize);
@@ -31,6 +33,24 @@ const app = new Vue({
         handleResize() {
             this.window.width = window.innerWidth;
             this.window.height = window.innerHeight;
-        }
+        },
+        showNext() {
+            // Shows next picture
+            if (this.counter === this.images.length - 1) {
+                return this.counter = 0, this.position = 'first ';
+            } else if (this.counter === this.images.length - 2) {
+                return this.counter += 1, this.position = 'last ';
+            }
+            return this.counter += 1, this.position = '';
+        },
+        showPrev() {
+            // Shows previous picture
+            if (this.counter === 0) {
+                return this.counter = this.images.length - 1, this.position = 'last ';
+            } else if (this.counter === 1) {
+                return this.counter -= 1, this.position = 'first ';
+            }
+            return this.counter -= 1, this.position = ' first ';
+        },
     }
 })
